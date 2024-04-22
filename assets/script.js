4f97acb60b52b8d6de2fc1087c4b1f307619eb4d
Fakhri17 <fakhricoffe@gmail.com>
2024-04-22 23:12:37 +0700

    feat: back to top

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,28 @@ String.prototype.EntoAr = function () {
 function changeToArabic(number) {
   return number.toString().EntoAr();
 }
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
